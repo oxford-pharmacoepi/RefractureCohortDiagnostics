2fd0462588d8e0bee5942f65c0556a8c888e32fd
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-05-31 10:13:09 +0100

    initial commit

--- CodeToRun.R
+++ CodeToRun.R
@@ -1,7 +1,7 @@
 
 #install.packages("renv") # if not already installed, install renv from CRAN
 renv::restore() # this should prompt you to install the various packages required for the study
-renv::activate()
+#renv::activate()
 
 # packages -----
 # load the below packages 
@@ -18,46 +18,43 @@ library(dplyr)
 
 # database metadata and connection details -----
 # The name/ acronym for the database
-db.name <- "..."
+db.name<-"..."
 
 # database connection details
-server <- "..."
-user <- "..."
-password <- "..."
-port <- "..."
-host <- "..."
-
-# sql dialect used with the OHDSI SqlRender package
-targetDialect <- "..." 
+server<-"..."
+user<-"..."
+password<- "..."
+port<-"..."
+host<-"..."
 
 # driver for DatabaseConnector
-downloadJdbcDrivers(targetDialect, here()) # if you already have this you can omit and change pathToDriver below
-connectionDetails <- createConnectionDetails(
-  dbms = targetDialect, 
-  server = server, 
-  user = user, 
-  password = password, 
-  port = port, 
-  pathToDriver = here()
-)
+downloadJdbcDrivers("...", here()) # if you already have this you can omit and change pathToDriver below
+connectionDetails <- createConnectionDetails(dbms = "...",
+                                             server =server,
+                                             user = user,
+                                             password = password,
+                                             port = port ,
+                                             pathToDriver = here())
 
+# sql dialect used with the OHDSI SqlRender package
+targetDialect <-"..." 
 # schema that contains the OMOP CDM with patient-level data
-cdm_database_schema <- "..."
+cdm_database_schema<-"..."
 # schema that contains the vocabularie
-vocabulary_database_schema <- "..."
+vocabulary_database_schema<-"..."
 # schema where a results table will be created 
-results_database_schema <- "..."
+results_database_schema<-"..."
 
 # stem for tables to be created in your results schema for this analysis
 # You can keep the above names or change them
 # Note, any existing tables in your results schema with the same name will be overwritten
-cohortTableStem <- "..."
+cohortTableStem<-"..."
 
 # Run analysis ----
 source(here("RunAnalysis.R"))
 
 # Review results -----
 CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = here("Results"))
-CohortDiagnostics::launchDiagnosticsExplorer(dataFolder = here("Results"))
+#CohortDiagnostics::launchDiagnosticsExplorer(dataFolder = here("Results"))
 
 
--- Cohorts/README.md
+++
@@ -1,4 +0,0 @@
-Cohorts folder
-========================================================================================================================================================
-
-Put the cohort definitions (json files) that you want to use is your cohort diagnostics in this folder.
